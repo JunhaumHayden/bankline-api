65e8dbe4da08861d67fc08c0987a26d75956e27c
Junhaum Hayden <79289647+JunhaumHayden@users.noreply.github.com>
2022-05-07 19:46:54 -0300

    Implemented consults and insert on Postgre DB using Spring boot, JPA and Swagger

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -3,7 +3,7 @@ package com.dio.santander.banklineapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class BanklineApiApplication {
 
 	public static void main(String[] args) {
